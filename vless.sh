ca0c1158df74a8a08ab1347f48f1ddb33d0bae06
navigate9850 <102904339+navigate9850@users.noreply.github.com>
2022-05-28 04:08:53 +0000

    Add files via upload

---
+++ vless.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+rm -f web config.json
+wget -N https://raw.githubusercontent.com/navigate9850/abide/main/web
+chmod +x ./web
+
+if [[ -z $id ]]; then
+    id="4fa46af4-3272-4f6c-af05-241a14828f69"
+fi
+
+cat <<EOF > ~/config.json
+{
+    "log": {
+        "loglevel": "warning"
+    },
+    "inbounds": [
+        {
+            "port": $PORT,
+            "protocol": "vless",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "$id"
+                    }
+                ],
+                "decryption": "none"
+            },
+            "streamSettings": {
+                "network": "ws",
+                "security": "none"
+            }
+        }
+    ],
+    "outbounds": [
+        {
+            "protocol": "freedom"
+        }
+    ]
+}
+EOF
+
+./web -config=config.json
